8338779c9a970f51cca308c3d9ddc91777aeede3
ArturSV1706 <86420182+ArturSV1706@users.noreply.github.com>
2024-01-08 14:58:23 -0300

    loading da tela de login arrumado

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,5 +1,26 @@
+
+<script>
+
+const loading = ref(true);
+
+if (process.client) {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 600) {
+        screen.value = 'desktop'
+    } else {
+        screen.value = 'mobile'
+    }
+    loading.value = false
+
+}
+
+</script>
 <template>
-    <div class="">
-        <slot/>
+    <div v-if="loading" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <img class="w-[10vw] animate-ping" src="../assets/icons/saffron_alt.svg" alt="">
+
+    </div>
+    <div v-if="!loading">
+        <slot />
     </div>
 </template>
