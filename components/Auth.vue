97ac2ec81d54923a402b30eec0b19f3727300a32
ArturSV1706 <86420182+ArturSV1706@users.noreply.github.com>
2024-01-10 10:45:13 -0300

    adicionado captcha

--- components/Auth.vue
+++ components/Auth.vue
@@ -1,4 +1,6 @@
 <script setup>
+  import VueHcaptcha from '@hcaptcha/vue3-hcaptcha';
+
 
 const authState = ref("entrar")
 const authError = ref("")
@@ -12,6 +14,7 @@ const router = useRouter()
 
 const { signIn, signUp, user } = useAuth()
 const screen = ref('mobile');
+const emit = defineEmits(['loading']);
 
 if (process.client) {
     const screenWidth = window.innerWidth;
@@ -33,6 +36,10 @@ const toggleAuthState = () => {
 }
 
 const handleSubmit = async () => {
+    let erro
+
+    emit('loading')
+
     try {
         if (authState.value === 'entrar') {
             await signIn({ email: input.email, password: input.password })
@@ -40,8 +47,22 @@ const handleSubmit = async () => {
         } else {
             await signUp({ email: input.email, password: input.password })
             showConfirmEmailMessage.value = true
+            emit('stopLoading')
         }
     } catch (err) {
+        if (err.message.includes('You must provide either an email')) {
+            // Do something when the error message matches the expected value
+            err.message = 'Você precisa adicionar um email e senha para fazer login.'
+        } else if (err.message.includes('Invalid login credentials')) {
+            // Do something when the error message matches the expected value
+            err.message = 'Credenciais inválidas, email ou senha incorretos'
+        } else if (err.message.includes('captcha verification process failed')) {
+            // Do something when the error message matches the expected value
+            err.message = 'Preencha o captcha para fazer login ou registrar-se'
+        }
+
+        console.log(err)
+        emit('stopLoading')
         authError.value = err.message
     }
 }
@@ -59,9 +80,11 @@ if (process.client) {
 <template>
     <div v-if="screen === 'desktop'">
         <div v-if="!showConfirmEmailMessage">
-            <div :class="`flex flex-col justify-evenly h-full w-full p-4 border-2 border-escuro`">
-                <h3 class="text-escuro mb-11 text-5xl font-aristotelica capitalize font-bold text-center">{{ authState }}
+            <div :class="`flex flex-col justify-evenly h-full min-w-[23vw] p-4 border-2 border-escuro`">
+                <h3 class="text-escuro mb-5 text-5xl font-aristotelica capitalize font-bold text-center">{{ authState }}
                 </h3>
+                <p class="px-2 mb-11 text-vermelho font-semibold animate-pulse text-center" v-if="authError">{{ authError }}</p>
+
                 <div class="flex flex-col items-center ">
                     <div class="relative z-0 w-[80%] mb-9 group">
 
@@ -72,7 +95,7 @@ if (process.client) {
                             class="pl-2 peer-focus:font-medium absolute text-sm text-verde_claro  duration-300 transform -translate-y-9 scale-75 top-[.7rem]  origin-[0] peer-focus:left-0 peer-focus:text-escuro peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-9 peer-focus:-pl-0">Email
                         </label>
                     </div>
-                    <div class="relative z-0 w-[80%] mb-9 group">
+                    <div class="relative z-0 w-[80%] mb-4 group">
 
                         <input type="password" v-model="input.password" name="floating_email" id="floating_email"
                             :class="` block py-1.5 px-2 w-full text-lg bg-${'verde'} text-claro bg-transparent transition-all border-0 border-b-4 border-verde appearance-none focus:outline-none focus:ring-0 focus:border-verde_claro peer`"
@@ -81,10 +104,12 @@ if (process.client) {
                             class="pl-2 peer-focus:font-medium absolute text-sm text-verde_claro  duration-300 transform -translate-y-9 scale-75 top-[.7rem]  origin-[0] peer-focus:left-0 peer-focus:text-escuro peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-9 peer-focus:-pl-0">Senha
                         </label>
                     </div>
+                    <vue-hcaptcha sitekey="bb7bc19b-29a9-4865-9bf9-e85c6ed115c8"></vue-hcaptcha>
+
 
                     <button
-                        class="px-8 py-2 w-[80%] self-center rounded transition-all capitalize border-2 bg-escuro text-claro font-bold hover:bg-verde hover:border-verde_claro hover:text-verde_claro"
-                        @click="handleSubmit">{{ authState }}</button>
+                        class="px-8 py-2 mt-2 w-[80%] self-center rounded transition-all capitalize border-2 bg-escuro text-claro font-bold hover:bg-verde hover:border-verde_claro hover:text-verde_claro"
+                        @click="handleSubmit()">{{ authState }}</button>
                     <!-- <input class="p-3 mb-4 bg-escuro text-claro outline" type="text" placeholder="email"
                             v-model="input.email">
                         <input type="password" placeholder="Password" v-model="input.password"> -->
@@ -92,7 +117,6 @@ if (process.client) {
 
                 </div>
                 <div class="w-[320px] mt-4">
-                    <p class="error" v-if="authError">{{ authError }}</p>
                     <p class="text-sm font-bold text-escuro text-center cursor-pointer" @click="toggleAuthState">{{
                         authState === "entrar" ? "Não possui uma conta? Clique para se registrar"
                         :
@@ -117,8 +141,10 @@ if (process.client) {
     <div v-if="screen === 'mobile'">
         <div v-if="!showConfirmEmailMessage">
             <div :class="`flex flex-col justify-evenly h-full w-[90vw] py-6 rounded-2xl border-escuro bg-claro space-y-10`">
-                <h3 class="text-escuro mb-6 text-5xl font-aristotelica capitalize font-bold text-center">{{ authState }}
+                <h3 class="text-escuro  text-5xl font-aristotelica capitalize font-bold text-center">{{ authState }}
                 </h3>
+                <p class="px-2 text-vermelho font-semibold animate-pulse text-center" v-if="authError">{{ authError }}</p>
+
                 <div class="flex flex-col items-center ">
                     <div class="relative z-0 w-[80%] mb-9 group">
 
@@ -138,9 +164,11 @@ if (process.client) {
                             class="pl-2 peer-focus:font-medium absolute text-sm text-verde_claro  duration-300 transform -translate-y-9 scale-75 top-[.7rem]  origin-[0] peer-focus:left-0 peer-focus:text-escuro peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-9 peer-focus:-pl-0">Senha
                         </label>
                     </div>
+                    <vue-hcaptcha sitekey="bb7bc19b-29a9-4865-9bf9-e85c6ed115c8"></vue-hcaptcha>
+
 
                     <button
-                        class="px-8 py-2 w-[80%] self-center rounded transition-all capitalize border-2 bg-escuro text-claro font-bold hover:bg-verde hover:border-verde_claro hover:text-verde_claro"
+                        class="px-8 py-2 mt-4 w-[80%] self-center rounded transition-all capitalize border-2 bg-escuro text-claro font-bold hover:bg-verde hover:border-verde_claro hover:text-verde_claro"
                         @click="handleSubmit">{{ authState }}</button>
                     <!-- <input class="p-3 mb-4 bg-escuro text-claro outline" type="text" placeholder="email"
                             v-model="input.email">
@@ -148,8 +176,7 @@ if (process.client) {
 
 
                 </div>
-                <div class="w-[100%] mt-4 text-center flex justify-center">
-                    <p class="error" v-if="authError">{{ authError }}</p>
+                <div class="w-[100%] mt-4 text-center flex flex-col justify-center items-center">
                     <p class="text-sm w-[70%] font-bold text-escuro text-center cursor-pointer" @click="toggleAuthState">{{
                         authState === "entrar" ? "Não possui uma conta? Clique para se registrar"
                         :
@@ -169,6 +196,7 @@ if (process.client) {
                     class="px-8 py-2 w-[80%] self-center rounded transition-all capitalize border-2 bg-escuro text-claro font-bold hover:bg-verde hover:border-verde_claro hover:text-verde_claro">Já
                     confirmei</button>
             </div>
+        </div>
     </div>
-</div></template>
+</template>
 
--- pages/login.vue
+++ pages/login.vue
@@ -2,6 +2,7 @@
 import useSupabase from '~~/composables/useSupabase';
 
 const screen = ref();
+const loading = ref()
 
 if (process.client) {
     const screenWidth = window.innerWidth;
@@ -23,8 +24,11 @@ definePageMeta({ layout: 'login' })
 
 <template>
     <div v-if="screen === 'desktop'" class="flex flex-col justify-center items-center h-screen w-screen">
+        <div v-if="loading" class="h-full w-full absolute flex justify-center items-center bg-claro opacity-60 z-[8]">
+        </div>
+        <div v-if="loading" class="w-[90px] h-[90px] z-10 border-[15px] rounded-[50%]  absolute border-verde border-t-escuro opacity-100 animate-spin"></div>
         <div class="flex shadow-2xl aspect-video">
-            <Auth class="flex flex-1" />
+            <Auth class="flex flex-1" @loading="loading = true" @stopLoading="loading = false" />
             <div class="flex flex-1 flex-col justify-evenly h-full w-full bg-escuro">
                 <img class="scale-[60%]" src="../assets/icons/logoLogin.svg" alt="">
             </div>
@@ -32,9 +36,12 @@ definePageMeta({ layout: 'login' })
 
     </div>
     <div v-if="screen === 'mobile'" class="h-screen flex flex-col justify-evenly items-center bg-escuro">
+        <div v-if="loading" class="h-full w-full absolute flex justify-center items-center bg-claro opacity-60 z-[8]">
+        </div>
+        <div v-if="loading" class="w-[90px] h-[90px] z-10 border-[15px] rounded-[50%]  absolute border-verde border-t-escuro opacity-100 animate-spin"></div>
         <div class="flex flex-col w-full justify-center items-center">
             <img class="w-[40%] " src="../assets/icons/logoLogin.svg" alt="">
         </div>
-        <Auth />
+        <Auth @loading="loading = true" @stopLoading="loading = false" />
     </div>
 </template>
